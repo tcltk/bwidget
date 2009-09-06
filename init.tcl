eff177d697743ecbeef87f33e09280c120f1f484
oberdorfer <oberdorfer>
2009-09-06 20:51:44 +0000

    BWidget::set_themedefaults "initialize" added

--- init.tcl
+++ init.tcl
@@ -1,3 +1,9 @@
+# ----------------------------------------------------------------------------
+#  init.tcl
+#  This file is part of Unifix BWidget Toolkit
+#  $Id: init.tcl,v 1.91 2009/09/06 20:51:44 oberdorfer Exp $
+# ----------------------------------------------------------------------------
+#
 namespace eval Widget {}
 proc Widget::_opt_defaults {{prio widgetDefault}} {
     if {$::tcl_version >= 8.4} {
@@ -38,3 +44,69 @@ bind Spinbox <<TraverseIn>> { %W selection range 0 end; %W icursor end }
 
 bind all <Key-Tab>       { Widget::traverseTo [Widget::focusNext %W] }
 bind all <<PrevWindow>>  { Widget::traverseTo [Widget::focusPrev %W] }
+
+namespace eval ::BWidget {
+
+    if {[lsearch -exact [font names] "TkTextFont"] < 0} {
+        catch {font create TkTextFont}
+        catch {font create TkDefaultFont}
+        catch {font create TkHeadingFont}
+        catch {font create TkCaptionFont}
+        catch {font create TkTooltipFont}
+
+        switch -- [tk windowingsystem] {
+            "win32" {
+                if {$::tcl_platform(osVersion) >= 5.0} {
+                    variable family "Tahoma"
+                } else {
+                    variable family "MS Sans Serif"
+                }
+                variable size 8
+
+                font configure TkDefaultFont -family $family -size $size
+                font configure TkTextFont    -family $family -size $size
+                font configure TkHeadingFont -family $family -size $size
+                font configure TkCaptionFont -family $family -size $size \
+                    -weight bold
+                font configure TkTooltipFont -family $family -size $size
+            }
+
+            "classic" - "aqua" {
+                variable family "Lucida Grande"
+                variable size 13
+                variable viewsize 12
+                variable smallsize 11
+
+                font configure TkDefaultFont -family $family -size $size
+                font configure TkTextFont    -family $family -size $size
+                font configure TkHeadingFont -family $family -size $smallsize
+                font configure TkCaptionFont -family $family -size $size \
+                    -weight bold
+                font configure TkTooltipFont -family $family -size $viewsize
+            }
+
+            "x11" {
+                if {![catch {tk::pkgconfig get fontsystem} fs]
+                    && [string equal $fs "xft"]} {
+                    variable family "sans-serif"
+                } else {
+                    variable family "Helvetica"
+                }
+                variable size -12
+                variable ttsize -10
+                variable capsize -14
+
+                font configure TkDefaultFont -family $family -size $size
+                font configure TkTextFont    -family $family -size $size
+                font configure TkHeadingFont -family $family -size $size \
+                    -weight bold
+                font configure TkCaptionFont -family $family -size $capsize \
+                    -weight bold
+                font configure TkTooltipFont -family $family -size $ttsize
+            }
+        }
+    }
+} ; ## namespace eval ::BWidget
+
+
+BWidget::set_themedefaults "initialize"
